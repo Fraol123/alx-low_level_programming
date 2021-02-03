983d0a336e3aa99ffa0a9c60fb3bde9a38d3d972
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-02-03 19:45:08 +0000

    edited printf

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,16 +8,11 @@ main()
   long int       long_int_num = 0;
   long long int  long_long_int_num = 0;
 
-  printf("the sizeof  char is: %d-byte\n", sizeof(char));
-  printf("the sizeof  ch   is :%d-byte\n", sizeof ch );
-  printf("the sizeof  int is: %d-byte \n", sizeof(int));
-  printf("the sizeof int_num is :%d-byte \n",sizeof int_num);
-  printf("the sizeof float is :%d-byte \n", sizeof(float));
-  printf("the sizeof flt_num is:%d-byte \n", sizeof flt_num);
-  printf("the sizeof long int is:%d-byte \n",sizeof(long int));
-  printf("the sizeof long_int_num is:%d-byte \n",sizeof long_int_num);
-  printf("the sizeof long long int is:%d-byte \n", sizeof(long long int));
-  printf("the sizeof long_long_int_num is :%d-byte \n",sizeof long_long_int_num);
+  printf("the sizeof a  char is: %d-byte\n", sizeof(char));
+  printf("the sizeof an int  is: %d-byte \n", sizeof(int));
+  printf("the sizeof a  float is : %d-byte \n", sizeof(float));
+  printf("the sizeof a  long int is:%d-byte \n",sizeof(long int));
+  printf("the sizeof a  long long int is:%d-byte \n", sizeof(long long int));
   return (0);
 
 }
