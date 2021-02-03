e188922f7ef315449d128a798dd7c98ee369175a
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-02-03 20:15:29 +0000

    added comment

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
-#include <string.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 
-int main () 
+int main(void)
 
 {
 
-  puts("Programming is like building a multilingual puzzle,\n");
+  puts("Programming is like building a multilingual puzzle\n");
   return (0);
 
 }
