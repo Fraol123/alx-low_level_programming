b0fdba451b5b683ea1574782b78e756ea9577ae0
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-02-04 18:45:04 +0000

    print num

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+
+long n;
+int base = 10
+findNumOfDigits(n, base);
+return (0);
+}
