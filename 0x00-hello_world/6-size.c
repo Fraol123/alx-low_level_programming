83150e341100933975592141f7920381ad30b9c3
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-02-04 19:31:28 +0000

    betty check

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,26 +2,25 @@
 /**
  *size off cheacker
  *
- * Main entery point 
- * 
+ * Main entery point
+ *
  *Return always 0 (success)
  */
 
 main()
 
 {
+char           ch = ' ';
+int            int_num = 0;
+float          flt_num = 0.0f;
+long int       long_int_num = 0;
+long long int  long_long_int_num = 0;
 
-  char           ch = ' ';
-  int            int_num = 0;
-  float          flt_num = 0.0f;
-  long int       long_int_num = 0;
-  long long int  long_long_int_num = 0;
-
-  printf("the sizeof a  char is: %d-byte\n", sizeof(char));
-  printf("the sizeof an int  is: %d-byte \n", sizeof(int));
-  printf("the sizeof a  float is : %d-byte \n", sizeof(float));
-  printf("the sizeof a  long int is:%d-byte \n",sizeof(long int));
-  printf("the sizeof a  long long int is:%d-byte \n", sizeof(long long int));
-  return (0);
+printf("sizeof a  char is : %d - byte\n", sizeof(char));
+printf("sizeof an int  is : %d - byte\n", sizeof(int));
+printf("sizeof a  float is : %d - byte\n", sizeof(float));
+printf("sizeof a  long int is : %d - byte\n", sizeof(long int));
+printf("sizeof a  long long int is : %d - byte\n", sizeof(long long int));
+return (0);
 
 }
