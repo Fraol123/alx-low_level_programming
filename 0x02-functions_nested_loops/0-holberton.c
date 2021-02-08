5ccccc514280d02d0da5aaf1c8a9783a4c5e2d04
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-02-08 11:24:20 +0000

    my header holberton

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ *Entery point
+ *
+ *Return: always 0 (sucess)
+ */
+
+int main(void)
+{
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+	return (0);
+}
