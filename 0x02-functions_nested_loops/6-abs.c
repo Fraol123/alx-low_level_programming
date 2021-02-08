5248b55561d8c749b34f0d68302753c1a3b6eaba
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-02-08 14:36:35 +0000

    absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * _abs - return absolute value
+ *
+ * @z: int checked
+ *
+ *Return: absolute Value
+ *
+ */
+
+int _abs(int)
+{
+	if (z < 0)
+	{
+		return (0 - z);
+	}
+	else
+	{
+		return (z);
+	}
+}
