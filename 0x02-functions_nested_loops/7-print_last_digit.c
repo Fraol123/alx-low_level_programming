9b755ee06440b45e8ef1835779173bd32aaae53c
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-02-08 17:29:02 +0000

    print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ *  print_last_digit - return last digit
+ *
+ *@n: int checked
+ *
+ * Return: last digit
+ */
+
+
+int print_last_digit(int n)
+{
+
+
+
+	return (n % 10);
+
+
+}
