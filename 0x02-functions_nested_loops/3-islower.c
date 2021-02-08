fe6784acaf40743330a318aee12d06ef405a701f
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-02-08 13:02:26 +0000

    checks for lowercase character

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ *_islower - check if int c is lower
+ *
+ * @c: int checked
+ * Return: 1 if lower,0 if upper
+ */
+
+int _islower(int c)
+{
+	if (c >= 97  &&  c  <=  122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
