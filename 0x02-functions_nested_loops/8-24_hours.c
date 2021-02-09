850ea35987bcdb126733176d1aee2c22d6ba12ef
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-02-09 18:06:04 +0000

    jackbour

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,53 +1,41 @@
+#include <stdio.h>
 #include "holberton.h"
-void jack_bauer(void)
+
+void jack_bauer(void);
+
 {
-	int h, m ;
-	for (h = 0; h < 24; h++)
 
-	{
-		for (m = 0; m < 60; m++)
-	{
-		if (h < 10)
-		{
-			if (m < 10)
-			{
-					_putchar((h / 10) +  '0');
-					_putchar((h % 10) +  '0');
-					_putchar(':');
-					_putchar((m / 10) + '0');
-					_putchar((m % 10) + '0');
-					_putchar('\n');
+int (HourTen, MinuteTen, HourOne, Minuteone);
+for (HourTen = '0'; HourTen <= '2'; HourTen++)
 
+for (HourOne = '0'; HourOne <= '9'; HourOne++)
+{
+for (MinuteTen = '0'; MinuteTen <= '5'; MinuteTen++)
+{
+for (Minuteone = '0'; Minuteone <= '9'; Minuteone++)
+{
 
-			}
-			else
-			{
+if (HourTen == 2 && HourOne >= 4)
+{
+break;
+}
 
-					_putchar((m / 10) + '0');
-					_putchar((m % 10) + '0');
+if (MinuteTen == 5 && Minuteone == 9)
+{
+break;
+}
+else
+{
+putchar (HourTen);
+putchar (HourOne);
+putchar (':');
+putchar (MinuteTen);
+putchar (Minuteone);
+putchar ('\n');
+}
 
-
-			}
-		}
-		else
-		{
-			if (m < 10)
-
-			{
-					_putchar((m / 10) + '0');
-					_putchar((m % 10 +  '0');
-
-
-
-			}
-			else
-			{
-					_putchar((m / 10) + '0');
-					_putchar((m % 10) +  '0')
-						_putchar('\n');
-
-			}
-			}
-	}
-		return (0);
+}
+}
+}
+return (0);
 }
