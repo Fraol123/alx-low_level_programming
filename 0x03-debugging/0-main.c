6870356a456326e322fdf1c259784305623748bf
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-02-10 15:12:46 +0000

    i checked

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,8 @@
 #include "holberton.h"
 
 /**
- * main - tests function that prints if integer is positive or negative
+ *main- positive_or_negative test
+ *@i: checked
  * Return: 0
  */
 void positive_or_negative(int i);
