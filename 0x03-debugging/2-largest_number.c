8d0fdca25c1afcba31d88f02e9097788e7a0ef19
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-02-10 14:06:56 +0000

    the largest

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,17 +12,18 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
+
+
 	if (a >= b && a >= c)
 	{
+
 		largest = a;
 	}
-
-	if (b >= a && b >= c)
+	else if (b >= a && b >= c)
 	{
 		largest = b;
 	}
-
-	if (c >= a && c >= b)
+	else
 	{
 		largest = c;
 	}
