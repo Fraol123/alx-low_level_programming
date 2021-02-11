cc1a0241a7f7e400640cb5c84ba47087bbe3beab
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-02-11 06:50:31 +0000

    num 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ *print_numbers- displays integer  0 -9
+ *
+ *Return: always 0
+ */
+void print_numbers(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		_putchar((num % 10) + '0');
+		_putchar('\n');
+	}
+}
