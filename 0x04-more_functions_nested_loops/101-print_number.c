fbfd8e22610df1b6152c089aefe0820db093532f
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-02-12 08:05:29 +0000

    print_number

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ *print_number- prints number
+ *@n:checked
+ *Return : nothing
+ */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n / 10 != 0)
+	{
+		print_number(n / 10);
+	}
+	_putchar(n % 10 + '0');
+}
