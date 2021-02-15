600209e85e71afd16b7ecc67ac09b4e388d1bf95
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-02-15 11:46:48 +0000

    Header

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -1,6 +1,6 @@
 /*my header*/
-int _putchar(char c)
-  void reset_to_98(int *n);
+int _putchar(char c);
+void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
