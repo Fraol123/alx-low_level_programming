abca40ff06d448d8320291af91955206d6eb2730
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-02-18 11:54:55 +0000

    leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ *leet- converts  letters to num
+ *@s: checked
+ *Return: s
+ */
+char *leet(char *s)
+{
+	int i, j;
+
+	char src[] = "aeotlAEOTL";
+	char dest[] = "4307143071";
+
+
+	for (i = 0; *(s + i); i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (src[j] == *(s + i))
+				*(s + i) = dest[j];
+		}
+	}
+	return (s);
+}
