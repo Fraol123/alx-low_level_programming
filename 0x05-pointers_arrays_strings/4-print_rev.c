27ba166e8276d7e22427e6d1df743d433fa0bcf1
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-02-20 18:58:47 +0000

    rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,25 +8,20 @@
 void print_rev(char *s)
 {
 
-	if (s)
+	int i, len;
+
+	i = 0;
+
+	len = 0;
+
+	while (s[len] != '\0')
 	{
-		int length = 0;
-
-		int i = 0;
-
-		int j;
-
-		for (j = 0; s[j] != '\0'; j++)
-		{
-			length++;
-		}
-
-		while (i <= length)
-		{
-
-			_putchar(s[length - i - 1]);
-			i++;
-		}
+		len++;
+	}
+	for (i = len - 1; i >= 0; i--)
+	{
+		_putchar(*(s + i));
 	}
 	_putchar('\n');
 }
+
