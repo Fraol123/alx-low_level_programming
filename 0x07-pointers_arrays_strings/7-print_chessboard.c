4858c12e3f3d6b0a6ef55977c9fc41b867296e53
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-02-22 07:19:44 +0000

    chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ *print_chessboard - prints a chessboard
+ *@: The chessboard to be printed
+ *
+ *Return: nothing
+ */
+
+void print_chessboard(char (*a)[8])
+{
+
+	int indx1, indx2;
+
+	for (indx1 = 0; a[indx1][7]; indx1++)
+	{
+		for (indx2 = 0; indx2 < 8; indx2++)
+
+			_putchar(a[indx1][indx2]);
+
+		_putchar('\n');
+
+        }
+}
