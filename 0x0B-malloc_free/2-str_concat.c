440aa35bd8496569e4037bf0665e3dddc6a0705f
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-03-02 14:15:50 +0000

    fixed betty

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -21,9 +21,9 @@ char *str_concat(char *s1, char *s2)
 
 	if (s2 == NULL)
 		s2 = "";
-	i = 0;
-	while (s1[i] || s2[i])
-		i++;
+
+	for (i = 0; s1[i] || s2[i]; i++)
+
 		len++;
 
 	concat = malloc(sizeof(char) * len);
