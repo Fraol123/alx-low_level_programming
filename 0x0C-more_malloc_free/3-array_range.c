280697ca3eb8c9e7e264f24bee94071998939c76
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-03-04 15:10:40 +0000

    array int

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ *array_range- cretes array of integer
+ *@max: last value of the array
+ *@min: first value of the array
+ *Return: array
+ */
+
+int *array_range(int min, int max)
+{
+	int *array, i, size;
+
+	size = max - min + 1;
+
+	if (min > max)
+		return (NULL);
+
+	array = malloc(sizeof(int) * size);
+	if (array == NULL)
+		return (NULL);
+
+	for (i = 0; i < max ; i++)
+	{
+		for (i = 0; i < size; i++)
+		{
+			array[i] = min++;
+		}
+	}
+	return (array);
+}
