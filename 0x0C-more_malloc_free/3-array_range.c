bdb0083e48d821c0e8be5727d5c9b9967bcdde65
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-03-05 08:38:22 +0000

    array

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -15,9 +15,9 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
-	size = max - min + 1;
+	size = max - min;
 
-	array = malloc(sizeof(int) * size);
+	array = malloc(sizeof(int) * (size + 1));
 	if (array == NULL)
 		return (NULL);
 
