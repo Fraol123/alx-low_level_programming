b14edad9e47d5fd5fb805fed36551a3585c0c6d9
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-03-08 14:28:41 +0000

    dog bio

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,21 +7,20 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
-
-		return;
-
-	if (d->name ==  NULL)
-		printf("Name:(nil)\n");
-	else
-		printf("Name: %s\n", d->name);
-	if (d->age < 0)
-		printf("Age:(nil)\n");
-	else
-		printf("Age: %f\n", d->age);
-	if (d->owner == NULL)
-		printf("owner:(nil)\n");
-	else
-		printf("owner:%s\n", d->owner);
+	if (d != NULL)
+	{
+		if (d->name !=  NULL)
+			printf("Name:(nil)\n");
+		else
+			printf("Name: %s\n", d->name);
+		if (d->age < 0)
+			printf("Age:(nil)\n");
+		else
+			printf("Age: %f\n", d->age);
+		if (d->owner != NULL)
+			printf("owner:(nil)\n");
+		else
+			printf("owner:%s\n", d->owner);
+	}
 }
 
