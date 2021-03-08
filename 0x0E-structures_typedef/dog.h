3a5a3c96d9d069319fa8cae006105fd5e8aa2f60
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-03-08 14:39:08 +0000

    new

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,12 +12,12 @@ typedef struct dog
 	char *name;
 	float age;
 	char *owner;
-} dog_w;
+} dog_t;
 
 
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_w *new_dog(char *name, float age, char *owner);
+dog_t *new_dog(char *name, float age, char *owner);
 
 #endif
