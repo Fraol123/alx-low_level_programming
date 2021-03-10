d40830d415f228bb239603e09ddf6f63b16fb63e
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-03-10 09:21:09 +0000

    name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ *print_name- prints name
+ *@name: the name to be printed
+ *@f: a pointer to a function that prints name
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != 0 || f != 0)
+		f(name);
+}
