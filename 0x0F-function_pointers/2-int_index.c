2b797418248222a3eede0fc1dcf9b339db04a562
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-03-10 13:22:44 +0000

    changed the logic operator

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,12 +11,18 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array == NULL || cmp == NULL)
-		return (-1);
-	for (i = 0; i < size; i++)
+	if (array && cmp && size > 0)
 	{
-		if (cmp(array[i] != 0))
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]))
+				break;
+		}
+
+		if (i < size)
+		{
 			return (i);
+		}
 	}
 	return (-1);
 }
