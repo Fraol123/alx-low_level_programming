95c7a6fca1ab49201a557325fe9f3c702fe9e0ce
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-03-10 09:02:54 +0000

    headder

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,7 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+
+#endif
