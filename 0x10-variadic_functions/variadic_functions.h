7926fac4f61c3f80872808cf905936dc00bc2f91
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-03-11 16:36:14 +0000

    VAR HEDEAR

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,19 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
+#include <stdarg.h>
+/**
+ *struct printer- prints struct
+ *@symbol: symbols for functions
+ *@func: pointer function
+ */
+typedef struct printer
+
+{
+	char *symbol;
+	void (*func)(va_list);
+
+} printer_f;
+
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
