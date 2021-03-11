207c8e409733e15a50de44dc9fddd095faf8a2ca
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-03-11 09:46:26 +0000

    HEADER

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,17 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
+/**
+ *struct printer- definig a printer
+ *@variable: a variable reprsenting a data type
+ *@print: a function ptr to func that prints
+ *        data crosponding to @variable
+ */
+
+typedef struct printer
+{
+	char *variable;
+	void (*print)(va_list arg);
+} printer_f;
 
 
 int sum_them_all(const unsigned int n, ...);
