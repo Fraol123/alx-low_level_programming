08dc2e39b4431709229ef05f50d088b600fbf82e
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-03-22 05:14:40 +0000

    My header and print nodes

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ *print_listint- a function that list all elemnts of list_t
+ *
+ *@h: The listint_t list
+ *Return: the number of nodes in h
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+
+		printf("%d\n", h->n);
+		nodes++;
+		h = h->next;
+	}
+	return (nodes);
+}
