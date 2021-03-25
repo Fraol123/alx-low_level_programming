d81a48e2fd0d7c3a2e857be3951e338478b455a2
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-03-25 17:49:45 +0000

    set

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ *set_bit-function that sets the value of a bit to 1
+ *@n: A ptr to a bit
+ *@index: the index to set the value
+ *Return: if an error occure -1 otherwise 1
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= (sizeof(unsigned long int) * 8))
+			return (-1);
+
+	*n  ^= (1 << index);
+
+	return (1);
+
+}
