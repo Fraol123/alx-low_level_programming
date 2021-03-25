a96bc598acea87ec6e979851475adf7c579b6ede
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-03-25 13:27:56 +0000

    to binnary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ *print_binary - function that prints the binary representation of a number
+ *@n: the number
+ */
+
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+		print_binary(n >> 1);
+			_putchar((n & 1) + '0');
+}
