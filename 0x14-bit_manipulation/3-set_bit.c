98ca79d05f1f1817171559b23878d499f775e80d
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-03-25 17:52:55 +0000

    betty error

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -8,10 +8,9 @@
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (index >= (sizeof(unsigned long int) * 8))
-			return (-1);
+		return (-1);
 
-	*n  ^= (1 << index);
+	*n ^= (1 << index);
 
 	return (1);
-
 }
