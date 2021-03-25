fa235db742a789a5e7424f0530e0013db721c3ff
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-03-25 12:31:30 +0000

    to unint

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ *binary_to_uint - convert a binary number to unsigned int
+ *@b:a pointer to a string of 0 & 1 chars
+ *Return: converted number or
+ *           if b is NULL or contains chars not 0 or 1 - 0.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int num = 0, mult = 1;
+	int len;
+
+	if (b == '\0')
+		return (0);
+
+	for (len = 0; b[len];)
+		len++;
+
+	for (len -= 1; len >= 0; len--)
+	{
+		if (b[len] != '0' && b[len] != '1')
+			return (0);
+
+		num += (b[len] - '0') * mult;
+		mult *= 2;
+	}
+
+	return (num);
+}
