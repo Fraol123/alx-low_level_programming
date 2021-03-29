f2d22a74606bb7e595396c087b732800f0e3ad1e
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-03-29 15:33:51 +0000

    my header

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -5,6 +5,8 @@
 #include <fcntl.h>  
 #include <unistd.h>
 
+#define BUF_SIZE 1024
+
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
