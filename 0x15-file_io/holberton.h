eb1853984b9c400bdddb0563511acb4fe290db5f
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-03-29 15:41:52 +0000

    header

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -1,8 +1,11 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
-#include<sys/types.h>
-#include<sys/stat.h>
-#include <fcntl.h>  
+
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
 
