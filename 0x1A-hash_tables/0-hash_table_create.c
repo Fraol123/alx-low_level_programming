7eb2879febae1d673c60c77d42b9fc0c3fea41e6
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-05-28 05:56:06 +0000

    creted hash table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+/**
+ *hash_table_create- creats a hash table
+ *@size : size of the hash table
+ *
+ *Return: the new hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht;
+
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+		return (NULL);
+
+	ht->size = size;
+	/*allocating memory for array*/
+	ht->array = malloc(sizeof(hash_node_t *) * size)
+	if (ht->array == NULL)
+	{
+		free(ht->array);
+		return (NULL);
+	}
+	return (ht);
+}
