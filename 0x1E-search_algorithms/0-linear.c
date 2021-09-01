c9149fa0f6c1ce9f9153f56da726ce58059f22ad
Fraol123 <fraoltesfaye.ft@gmail.com>
2021-09-01 06:41:03 +0000

    fixed error

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -11,18 +11,18 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-	size_t i;
-	int tmp;
 
-	if (array != NULL)
+	size_t i;
+
+	if (array == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
 	{
-		for (i = 0; i < size; i++)
-		{
-			tmp = array[i];
-			printf("value checked array[%lu] = [%d]\n", i, tmp);
-			if (tmp == value)
-				return (i);
-		}
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
 	}
+
 	return (-1);
 }
